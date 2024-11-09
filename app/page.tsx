249848eeada30fd4bf1e41656ce850d0a4db5bd3
unknown <mansoor.ahmed11521@gmail.com>
2024-11-09 06:27:11 +0500

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return ( 
+    <div>
+      <h1>Hello to the World Of Next JS</h1>
+      <h3> You're most welcome by Mansoor who's a student of Monday (2-5)pm</h3>
+    </div>
+  )
+}
